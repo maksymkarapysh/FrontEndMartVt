061057a2850d1f0f02e879e8f6d4b6db1c139565
Mistok <Mistreated@ukr.net>
2018-07-19 16:52:12 +0300

    homework #4 css grid

---
+++ Bilous_Dmitry/HW_HTML5&CSS3/Homework4 (css grid)/HTML5&CSS3_Adv_Bilous_4.js
@@ -0,0 +1,23 @@
+
+
+window.onload = function () {
+	var points = document.getElementsByClassName('enemie');
+	points[0].addEventListener('click', showMassage);
+// Функция покажет сообщение
+	function showMassage(event){
+		var alert =  document.getElementsByClassName('alert')[0];
+			alert.classList.add('animateShow');
+/* После того, как сообщение показано запускаются таймеры,
+ которые уберут его в течении 5 секунд и очистят класс лист */
+			setTimeout(animateHidding, 4000);
+			setTimeout(clear,5000);
+		function animateHidding(){
+			alert.classList.add('animateHide')
+			
+		}
+		function clear(){
+			alert.classList.remove('animateShow', 'animateHide');
+		}
+	}
+}
+
