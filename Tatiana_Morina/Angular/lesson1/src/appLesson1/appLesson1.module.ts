24de56fa89cadb6abb62df70982a8264557ba80e
morinata <morina.t.a@gmail.com>
2018-08-27 19:55:10 +0300

    update HomeWork

---
+++ Tatiana_Morina/Angular/lesson1/src/appLesson1/appLesson1.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppLesson1Component } from './appLesson1.component';
+
+@NgModule({
+  declarations: [
+    AppLesson1Component
+  ],
+
+  imports: [
+    BrowserModule
+  ],
+
+  providers: [],
+
+  bootstrap: [AppLesson1Component]
+})
+
+export class AppModule {
+  
+ }
