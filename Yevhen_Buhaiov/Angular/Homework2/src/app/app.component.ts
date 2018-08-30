a337e4de6d7910f0188a374695489e42ed4b0027
Yevhen <eugene108@gmail.com>
2018-08-30 17:40:31 +0300

    homework without EventEmitter

---
+++ Yevhen_Buhaiov/Angular/Homework2/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'homework2';
+  //consInf(Id) {
+   // console.log ("Deleted id:"+ Id );
+  }
+
+
---
+++ Yevhen_Buhaiov/Angular/Homework2/src/app/my-table/my-table.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { EventEmitter } from 'events';
+
+const Products = [
+  { id: 1, name: 'product 1', price: 100 },
+  { id: 2, name: 'product 2', price: 200 },
+  { id: 3, name: 'product 3', price: 300 },
+  { id: 4, name: 'product 4', price: 400 },
+  { id: 5, name: 'product 5', price: 500 },
+  { id: 6, name: 'product 6', price: 600 },
+  { id: 7, name: 'product 7', price: 700 },
+  { id: 8, name: 'product 8', price: 800 },
+  { id: 9, name: 'product 9', price: 900 },
+  { id: 10, name: 'product 10', price: 1000}
+]; 
+
+@Component({
+  selector: 'app-my-table',
+  templateUrl: './my-table.component.html',
+  styleUrls: ['./my-table.component.css']
+})
+export class MyTableComponent implements OnInit {
+  products = Products;
+  @Input () rows;
+  //@Output () delRow = new EventEmitter();
+
+  constructor( ) {
+
+  }
+
+  ngOnInit() {
+    this.products = this.products.slice(0, this.rows);
+  }
+  deleteItem (rowIndex) {
+    this.products.splice(rowIndex,1);
+    //this.delRow.emit(rowIndex);
+  }
+    
+
+
+}
